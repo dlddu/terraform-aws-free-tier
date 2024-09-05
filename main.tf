e84b49d5ab45be3c4d3bcb1c227f21f5cdff52f1
dlddu <39251873+dlddu@users.noreply.github.com>
2024-09-05 09:32:10 +0900

    feat: Add AWS account alias and budget configuration
    This commit adds the necessary Terraform code to configure an AWS budget. It includes the definition of variables for the account alias, budget description, monthly cost limit, and subscriber email address. The code also creates an AWS IAM account alias and an AWS budget resource with the specified configuration.
    
    The purpose of this change is to enable budget monitoring and cost control for the AWS account.
    
    Note: Please remove this note before committing.

---
+++ main.tf
@@ -0,0 +1,36 @@
+variable "account_alias" {
+  type = string
+}
+
+resource "aws_iam_account_alias" "this" {
+  account_alias = var.account_alias
+}
+
+variable "budget_description" {
+  type = string
+}
+
+variable "monthly_cost_limit_in_usd" {
+  type = number
+}
+
+variable "subscriber_email_address" {
+  type = string
+}
+
+resource "aws_budgets_budget" "this" {
+  name              = var.budget_description
+  budget_type       = "COST"
+  limit_amount      = var.monthly_cost_limit_in_usd
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2024-01-01_00:00"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.subscriber_email_address]
+  }
+}
